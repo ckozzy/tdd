29b1b553c1adee784548a4750904dd197afd1142
C Kelly Osborn <kelly@shadetree.biz>
2014-07-22 17:25:20 -0600

    inserting test for alignment while reverting back to not centered

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -39,8 +39,10 @@ class NewVisitorTest(LiveServerTestCase):
       # "1: Buy peacock feathers" as an item in a to-do list table
      inputbox.send_keys(Keys.ENTER)
      edith_list_url = self.browser.current_url
+     self.assertRegex(edith_list_url, '/lists/.+')
+     self.check_for_row_in_list_table('1: Buy peacock feathers')
      
- ##    time.sleep(10)
+
      self.assertRegex(edith_list_url, '/lists/.+')
      """ failing above will not work till nd chapter 6"""
 
@@ -57,10 +59,51 @@ class NewVisitorTest(LiveServerTestCase):
      self.check_for_row_in_list_table('1: Buy peacock feathers')
      self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
      
-     # Edith wonders if the siet will remember her list.  Then she sees 
-     # that the site has generated a unique URL for her -- ther is some 
-     # eplanatory text to that effect.
-     self.fail('Finish the test!')
+     # Now a new user, Francis, comes along to the site.
      
-     # Se visits that URL - her to-do list is still there.
+     ## We use a new browser session to make sure that no information 
+     ## of Edith's is coming through from cookies etc
+     
+     self.browser.quit()
+     self.browser = webdriver.Firefox()
+     
+     # Francis visits the home page. There is no sign of Ediths's
+     # List
+     self.browser.get(self.live_server_url)
+     page_text = self.browser.find_element_by_tag_name('body').text
+     self.assertNotIn('By peacock feather', page_text)
+     self.assertNotIn('make a fly', page_text)
+     
+     # Francis starts a new list by entering a new item. he
+     # is less interesting thatn Edith...
+     inputbox = self.browser.find_element_by_id('id_new_item')
+     inputbox.send_keys('Buy milk')
+     inputbox.send_keys(Keys.ENTER)
+
+     
+     # Francis gets his own unique current_url
+     francis_list_url = self.browser.current_url
+     page_text = self.browser.find_element_by_tag_name('body').text
+     self.assertRegex(francis_list_url, '/lists/.+')
+     self.assertIn('milk', page_text)
+     
+     # Satisfied they both go back to sleep
+
+  def test_layout_and_styling(self):
+     # Edith goes to the home page
+     self.browser.get(self.live_server_url)
+     self.browser.set_window_size(1024, 768)
+     
+     # She notices the input box is nicely centered
+     inputbox = self.browser.find_element_by_id('id_new_item')
+     self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
+  
+     
+     # She starts a new list and sees the input is nicely
+     # centered there too
+     inputbox.send_keys('testing\n')
+     inputbox = self.browser.find_element_by_id('id_new_item')
+     self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
+
+     #time.sleep(10) 
 
