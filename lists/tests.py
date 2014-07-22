9ac229deb71ace6ffe0b9a36c4b1526ddf7fadae
C Kelly Osborn <kelly@shadetree.biz>
2014-07-22 11:38:44 -0600

    ch6 pointing our forms at the new URL

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,8 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
+from lists.views import home_page
 
 
 
